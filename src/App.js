7f41f66779c839ddfac69905d7d4c55428c27884
YashGupta018 <ygupta2001@gmail.com>
2023-02-10 11:32:00 +0530

    Created canvas & default functions (const)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,73 @@
-import React from "react";
+import React, {useState, useRef, useEffect} from "react";
+import { useInterval } from "./useInterval";
+import { 
+  CANVAS_SIZE,
+  SNAKE_START,
+  APPLE_START,
+  SCALE,
+  SPEED,
+  DIRECTIONS
+} from "./constants";
 
-const App = () => <div>Snake Game</div>
-  
-export default App;
+const App = () => {
+
+  const startGame = () => {
+
+
+
+  }
+
+  const endGame = () => {
+
+
+
+  }
+
+  const moveSnake = () => {
+
+
+
+  }
+
+  const createApple = () => {
+
+
+
+  }
+
+  const checkCollision = () => {
+
+
+
+  }
+
+  const checkAppleCollision = () => {
+
+
+
+  }
+
+  const gameLoop = () => {
+
+
+
+  }
+
+  useEffect(() => {
+
+  }, [snake, apple, gameOver])
+
+  return (
+    <div role="button" tabIndex="0" onKeyDown={e => moveSnake(e)}>
+      <canvas
+        style={{border: "1px solid black"}}
+        ref={canvasRef}
+        width={`${CANVAS_SIZE[0]}px`}
+        height={`${CANVAS_SIZE[0]}px`}
+        />
+    </div>
+  )
+
+}
+
+export default App
