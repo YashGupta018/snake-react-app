5be341aa50b104666d0baa1cbbe152c5e36bc62f
YashGupta018 <ygupta2001@gmail.com>
2023-02-10 20:34:00 +0530

    Snake Crawling

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ const App = () => {
   const [snake, setSnake] = useState(SNAKE_START);
   const [apple, setApple] = useState(APPLE_START);
   const [dir, setDir] = useState(0, -1);
-  const [speed, setSpeed] = useState(null);
+  const [speed, setSpeed] = useState(100);
   const [gameOver, setGameOver] = useState(false);
 
   const startGame = () => {
@@ -30,11 +30,7 @@ const App = () => {
 
   }
 
-  const moveSnake = () => {
-
-
-
-  }
+  const moveSnake = ({ keyCode }) => keyCode >= 37 && keyCode <= 40 && setDir(DIRECTIONS[keyCode]);
 
   const createApple = () => {
 
@@ -56,7 +52,11 @@ const App = () => {
 
   const gameLoop = () => {
 
-
+    const snakeCopy = JSON.parse(JSON.stringify(snake)); // (snake copy) from snake object coz snake is a multi-dimensional array
+    const newSnakeHead = [snakeCopy[0][0] + dir[0], snakeCopy[0][1] + dir[1]];
+    snakeCopy.unshift(newSnakeHead);
+    snakeCopy.pop();
+    setSnake(snakeCopy);
 
   }
 
@@ -71,6 +71,8 @@ const App = () => {
     context.fillRect(apple[0], apple[1], 1, 1);
 
   }, [snake, apple, gameOver])
+
+  useInterval(() => gameLoop(), speed);
 
   return (
     <div role="button" tabIndex="0" onKeyDown={e => moveSnake(e)}>
